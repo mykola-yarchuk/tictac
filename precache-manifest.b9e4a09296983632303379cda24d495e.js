2c92142bc5c050c452cc197dcd4ff402afb9c9eb
Nick <nikolyaoooyartchuk@gmail.com>
2020-05-01 17:20:11 +0300

    Updates

---
+++ precache-manifest.b9e4a09296983632303379cda24d495e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80f214fccba6c0ffe04b2bff5e3f7d6a",
+    "url": "/tictac/index.html"
+  },
+  {
+    "revision": "05041c3cffd9c2a9540d",
+    "url": "/tictac/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "17c79084c1e20441ba34",
+    "url": "/tictac/static/js/2.a489c2db.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tictac/static/js/2.a489c2db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05041c3cffd9c2a9540d",
+    "url": "/tictac/static/js/main.083be829.chunk.js"
+  },
+  {
+    "revision": "9eff7804c8c7a07b820c",
+    "url": "/tictac/static/js/runtime-main.6bb0d448.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tictac/static/media/logo.5d5d9eef.svg"
+  }
+]);
